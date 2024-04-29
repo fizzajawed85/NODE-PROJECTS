198a624b257b89c9cc61fba14a64b2baa02b1443
fizzajawed85 <angelfizza85@gmail.com>
2024-04-29 20:54:52 +0500

    first commit

---
+++ project01_Number_Guessing_Game/index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Code With Fizza - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess Number(Number Limit from 1 to 5):",
+    },
+]);
+// Conditional Statement.
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation! You guess a correct number");
+}
+else {
+    console.log("Sorry! You guess a wrong number");
+}
+;
